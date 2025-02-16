1e4d250b45e1094f1d1b12433f83dbef5e357abc
Aashish Swarnkar <eceaashish.swarnkar@gmail.com>
2025-02-16 14:13:23 +0530

    Data version 2

--- src.py
+++ src.py
@@ -2,19 +2,32 @@ import pandas as pd
 from pathlib import Path
 
 
-# create a sample DatFrame 
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [23, 32, 26],
-    'City': ['New York', 'Seattle', 'California']
-}
+def save_data(df):
+    data_dir = Path('data')
+    if not data_dir.is_dir():
+        data_dir.mkdir(parents=True, exist_ok=True)
 
-df = pd.DataFrame(data)
-data_dir = Path('data')
+    file_path = data_dir/'data.csv'
+    df.to_csv(file_path, index = False)
+    print(f'Data file saved to {file_path}')
 
-if not data_dir.is_dir():
-    data_dir.mkdir(parents=True, exist_ok=True)
+def create_data():
 
-file_path = data_dir/'data.csv'
-df.to_csv(file_path, index = False)
-print(f'Data file saved to {file_path}')
+    # create a sample DatFrame 
+    data = {
+        'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [23, 32, 26],
+        'City': ['New York', 'Seattle', 'California']
+    }
+
+    df = pd.DataFrame(data)
+
+    save_data(df)
+
+    # Adding new row to dataframe
+    new_data = {'Name': 'Robert', 'Age': 55, 'City': 'Boston'}
+    df.loc[len(df.index), :] = new_data
+    save_data(df)
+
+if __name__ == '__main__':
+    create_data()
